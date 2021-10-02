d52576d56f83db2badfed72cd2bacc5be970d8ed
Artyu <65313164+Artyu24@users.noreply.github.com>
2021-10-02 15:50:50 +0200

    Deplacement sur Plateau 5x5

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Transform playerPosition;
+    float playerPositionX;
+    float playerPositionY;
     Vector2[,] plateau = new Vector2[5,5];
 
     private void Start()
     {
         playerPosition = this.transform;
+
+        //Intégration de chaque coord dans chaque partie du tableau
         for (int i = 0; i < 5; i++)
         {
             for (int j = 0; j < 5; j++)
@@ -17,26 +21,46 @@ public class Player : MonoBehaviour
                 plateau[i, j] = new Vector2(i,j);
             }
         }
+
+        //Set Pos default player
         playerPosition.position = plateau[0, 0];
+        playerPositionX = playerPosition.position.x;
+        playerPositionY = playerPosition.position.y;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Z))
         {
-            playerPosition.position = new Vector2(playerPosition.position.x, playerPosition.position.y + 1);
+            if (playerPositionY != 4)
+            {
+                playerPosition.position = plateau[(int)playerPositionX, (int)playerPositionY + 1];
+                playerPositionY++;
+            }
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            playerPosition.position = new Vector2(playerPosition.position.x, playerPosition.position.y - 1);
+            if (playerPositionY != 0)
+            {
+                playerPosition.position = plateau[(int)playerPositionX, (int)playerPositionY - 1];
+                playerPositionY--;
+            }
         }
         else if (Input.GetKeyDown(KeyCode.Q))
         {
-            playerPosition.position = new Vector2(playerPosition.position.x - 1, playerPosition.position.y);
+            if (playerPositionX != 0)
+            {
+                playerPosition.position = plateau[(int)playerPositionX - 1, (int)playerPositionY];
+                playerPositionX--;
+            }
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            playerPosition.position = new Vector2(playerPosition.position.x + 1, playerPosition.position.y);
+            if(playerPositionX != 4)
+            {
+                playerPosition.position = plateau[(int)playerPositionX + 1, (int)playerPositionY];
+                playerPositionX++;
+            }
         }
     }
 }
