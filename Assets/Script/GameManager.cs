a7ecbad5d9bdd26f7588dc5b9ba1f43e982a79f3
Artyu <65313164+Artyu24@users.noreply.github.com>
2021-10-02 23:23:26 +0200

    Systeme GameOver for A to B tableau

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
     [Header("Plateau")]
     public static int taillePlateau = 6;
-
     public static int positionPlayerX = 0;
     public static int positionPlayerY = 0;
 
-
     [Header("Enemy")] public static float tempsDeReaction = 1;
 
+    [SerializeField]
+    GameObject panelGameOver;
+
+    public void Start()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    public void GameOver()
+    {
+        panelGameOver.SetActive(true);
+    }
 }
