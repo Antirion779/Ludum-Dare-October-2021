ca13bb087d354d346e57f791cfda0ec7c2b35010
Artyu <65313164+Artyu24@users.noreply.github.com>
2021-10-02 15:18:58 +0200

    Deplacement

---
+++ Assets/Arthur/Script/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Transform playerPosition;
+    Vector2[,] plateau = new Vector2[5,5];
+
+    private void Start()
+    {
+        playerPosition = this.transform;
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                plateau[i, j] = new Vector2(i,j);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            playerPosition.position = new Vector2(playerPosition.position.x, playerPosition.position.y + 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            playerPosition.position = new Vector2(playerPosition.position.x, playerPosition.position.y - 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            playerPosition.position = new Vector2(playerPosition.position.x - 1, playerPosition.position.y);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            playerPosition.position = new Vector2(playerPosition.position.x + 1, playerPosition.position.y);
+        }
+    }
+}
